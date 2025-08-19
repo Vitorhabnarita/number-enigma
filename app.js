834d4b0ccb603a77b32cd273a30f80a9109c0f17
Vitor Hiroshi <vitor.narita@aluno.unievangelica.edu.br>
2025-08-19 08:29:56 -0300

    corrigindo bug novamente

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(seletor, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
 exibirMensagemInicial();
